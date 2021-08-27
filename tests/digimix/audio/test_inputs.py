5a1f7533a8f50ebbeee4c20e5a939461f2c47023
Christian Kohlstedde <christian@kohlsted.de>
2021-08-27 17:12:06 +0200

    Improve JackClient inputs

--- tests/digimix/audio/test_inputs.py
+++ tests/digimix/audio/test_inputs.py
@@ -1,9 +1,36 @@
 from digimix.audio.base import AudioMode
-from digimix.audio.inputs import SingleJackClientInput
+from digimix.audio.inputs import MultiJackClientInput, SingleJackClientInput
 
 
-def test_input_patch_panel():
+def test_SingleJackClientInput():
     in_patch_panel = SingleJackClientInput(
+        'test',
+        (
+            ('music', AudioMode.STEREO),
+            ('mic', AudioMode.MONO),
+        )
+    )
+    desc = in_patch_panel.pipeline_description
+    desc += f"""
+    audiomixer name=mix
+
+    src-music.
+    ! audiopanorama
+    ! mix.
+    src-mic.
+    ! audiopanorama
+    ! mix.
+
+    mix. ! jackaudiosink connect=0 name=jacksink client-name=GstSink
+    """
+
+    print(desc)
+    desc = ' '.join(desc.replace('\n', ' ').split()).replace('(', '\\(').replace(')', '\\)')
+    print(desc)
+
+
+def test_MultiJackClientInput():
+    in_patch_panel = MultiJackClientInput(
         'test',
         (
             ('music', AudioMode.STEREO),
