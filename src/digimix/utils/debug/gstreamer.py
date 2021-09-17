58173afc07a4c76f9d283d60c8241c5a7694b1e2
Christian Kohlstedde <christian@kohlsted.de>
2021-09-17 19:17:53 +0200

    Build example pipeline

---
+++ src/digimix/utils/debug/gstreamer.py
@@ -0,0 +1,56 @@
+# based on https://github.com/voc/voctomix/blob/3156f3546890e6ae8d379df17e5cc718eee14b15/vocto/debug.py
+
+import logging
+import os
+
+from digimix.audio import Gst
+
+log = logging.getLogger('vocto.debug')
+
+
+def gst_generate_dot(pipeline, name):
+    if 'GST_DEBUG_DUMP_DOT_DIR' in os.environ:
+        dotfile = os.path.join(os.environ['GST_DEBUG_DUMP_DOT_DIR'], "%s.dot" % name)
+        log.debug("Generating DOT image of pipeline '{name}' into '{file}'".format(name=name, file=dotfile))
+        Gst.debug_bin_to_dot_file(pipeline, Gst.DebugGraphDetails(15), name)
+
+
+gst_log_messages_lastmessage = None
+gst_log_messages_lastlevel = None
+gst_log_messages_repeat = 0
+
+
+def gst_log_messages(level):
+    gst_log = logging.getLogger('Gst')
+
+    def log(level, msg):
+        if level == Gst.DebugLevel.WARNING:
+            gst_log.warning(msg)
+        if level == Gst.DebugLevel.FIXME:
+            gst_log.warning(msg)
+        elif level == Gst.DebugLevel.ERROR:
+            gst_log.error(msg)
+        elif level == Gst.DebugLevel.INFO:
+            gst_log.info(msg)
+        elif level == Gst.DebugLevel.DEBUG:
+            gst_log.debug(msg)
+
+    def log_function(category, level, file, function, line, object, message, *user_data):
+        global gst_log_messages_lastmessage, gst_log_messages_lastlevel, gst_log_messages_repeat
+
+        msg = message.get()
+        if gst_log_messages_lastmessage != msg:
+            if gst_log_messages_repeat > 2:
+                log(gst_log_messages_lastlevel, "%s [REPEATING %d TIMES]" % (gst_log_messages_lastmessage, gst_log_messages_repeat))
+
+            gst_log_messages_lastmessage = msg
+            gst_log_messages_repeat = 0
+            gst_log_messages_lastlevel = level
+            log(level, "%s: %s (in function %s() in file %s:%d)" % (object.name if object else "", msg, function, file, line))
+        else:
+            gst_log_messages_repeat += 1
+
+    Gst.debug_remove_log_function(None)
+    Gst.debug_add_log_function(log_function, None)
+    Gst.debug_set_default_threshold(level)
+    Gst.debug_set_active(True)
--- src/main.py
+++ src/main.py
@@ -2,34 +2,84 @@ import threading
 import time
 
 from digimix.audio import GLib, Gst
+from digimix.audio.base import AudioMode
+from digimix.audio.buses import MasterBus
 from digimix.audio.channels import FaderChannel
+from digimix.audio.io.jack import SingleJackClientInput, SingleJackClientOutput
 from digimix.utils.debug.gstreamer import gst_generate_dot
 
 if __name__ == '__main__':
-    el = FaderChannel(name="mic")
+    src = SingleJackClientInput(
+        name="main_in",
+        conf=(
+            ("mic1", AudioMode.MONO),
+            ("music", AudioMode.STEREO),
+            ("drums", AudioMode.STEREO),
+            ("bass", AudioMode.MONO),
+            ("guitar", AudioMode.MONO),
+        )
+    )
 
-    desc = f"jackaudiosrc connect=0 client-name=FaderChannelIn ! {el.sink[0]}. "
-    desc += el.pipeline_description
-    desc += f"{el.src[0]}. ! jackaudiosink connect=0 client-name=FaderChannelOut"
+    mic1 = FaderChannel(
+        name="mic1",
+    )
+    music = FaderChannel(name="music")
+    drums = FaderChannel(name="drums")
+    bass = FaderChannel(name="bass")
+    guitar = FaderChannel(name="guitar")
+
+    master = MasterBus(name="master", inputs=[
+        mic1.src[0],
+        music.src[0],
+        drums.src[0],
+        bass.src[0],
+        guitar.src[0],
+    ])
+
+    out = SingleJackClientOutput(
+        name="main_out",
+        conf=(
+            ("main", AudioMode.STEREO),
+        )
+    )
+
+    desc = ""
+    desc += src.pipeline_description + "\n"
+    desc += mic1.pipeline_description + "\n"
+    desc += music.pipeline_description + "\n"
+    desc += drums.pipeline_description + "\n"
+    desc += bass.pipeline_description + "\n"
+    desc += guitar.pipeline_description + "\n"
+    desc += master.pipeline_description + "\n"
+    desc += out.pipeline_description + "\n"
+
+    desc += f"{src.src[0]}. ! {mic1.sink[0]}." + "\n"
+    desc += f"{src.src[1]}. ! {music.sink[0]}." + "\n"
+    desc += f"{src.src[2]}. ! {drums.sink[0]}." + "\n"
+    desc += f"{src.src[3]}. ! {bass.sink[0]}." + "\n"
+    desc += f"{src.src[4]}. ! {guitar.sink[0]}." + "\n"
+    desc += f"{master.src[0]}. ! {out.sink[0]}." + "\n"
+
+    print(desc)
 
     main = GLib.MainLoop()
     pipeline = Gst.parse_launch(desc)  # type: Gst.Pipeline
     assert pipeline
     pipeline.set_state(Gst.State.PLAYING)
-    el.attach_pipeline(pipeline)
+    music.attach_pipeline(pipeline)
 
 
     def threader():
         time.sleep(2)
         gst_generate_dot(pipeline, "1")
-        el.cut = True
+        music.cut = True
         gst_generate_dot(pipeline, "2")
         time.sleep(0.5)
-        el.cut = False
-        el.pan = -0.5
+        music.cut = False
+        music.pan = -0.5
         gst_generate_dot(pipeline, "3")
         time.sleep(0.5)
-        el.fader_db = -10
+        music.fader_db = -10
         gst_generate_dot(pipeline, "4")
         time.sleep(2)
         main.quit()
